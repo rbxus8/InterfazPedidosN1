afaa9b7757d32929a2465800c84e347893d02e3b
julian carvajalino <juliancarvajalino8@gmail.com>
2025-05-31 23:15:39 -0500

    fix(php):correccion error echo

--- delete_producto.php
+++ delete_producto.php
@@ -10,9 +10,8 @@ $stmt->bind_param("i", $id);
 if ($stmt->execute()) {
     echo "Producto eliminado exitosamente.";
 } else {
-    echo "Error al eliminar producto: " . $conn->error;
+    echo "Error al eliminar producto: " . $stmt->error;
 }
 
 $stmt->close();
 $conn->close();
-?>
