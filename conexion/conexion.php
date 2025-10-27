37910cb3c05e0704e3b20fdc00ac0545c193add8
jeyxus-ui <TU@gmail.com>
2025-10-26 20:19:52 -0500

    fix: verificación para no abrir múltiples conexiones

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,13 +1,15 @@
 <?php
-$host = "b1xbvdktlo20sdr39wbi-mysql.services.clever-cloud.com";
-$usuario = "udqgmhwed2gjzprz";
-$contrasena = "5pDRSAyLkyoXQW28HNBK";
-$baseDatos = "b1xbvdktlo20sdr39wbi";
+if (!isset($conexion)) {
+    $host = "b1xbvdktlo20sdr39wbi-mysql.services.clever-cloud.com";
+    $usuario = "udqgmhwed2gjzprz";
+    $contrasena = "5pDRSAyLkyoXQW28HNBK";
+    $baseDatos = "b1xbvdktlo20sdr39wbi";
 
-// Crear la conexión
-$conexion = new mysqli($host, $usuario, $contrasena, $baseDatos);
+    // Crear la conexión
+    $conexion = new mysqli($host, $usuario, $contrasena, $baseDatos);
 
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
 }
